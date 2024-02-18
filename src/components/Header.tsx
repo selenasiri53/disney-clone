ddca9599cfb36caf043dd846f42006229abf7489
selenasiri53 <selenasiri53@gmail.com>
2024-02-18 16:14:10 -0500

    disney image logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header>
       <Link href="/">
-        Click me
+        <Image
+        src="https://links.papareact.com/a943ae"
+        alt="Logo" 
+        width={120}
+        height={100}
+        className="cursor-pointer" 
+         />
       </Link>
     </header>
   )
