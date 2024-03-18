f30c3c2a6c3a70c31e5351cb90fc93f36a98127c
selenasiri53 <selenasiri53@gmail.com>
2024-03-18 13:43:39 -0400

    form notes

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form";
-// import * as z from "zod"
+
 import { z } from "zod"
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
@@ -56,42 +56,4 @@ export default function SearchInput() { // redirects the user after search
           )
 }
 
-
-//     //1. define your form
-//     const form = useForm<z.infer<typeof formSchema>>({
-//         resolver: zodResolver(formSchema),
-//         defaultValues: {
-//             input: "", // matches our schema
-//         }
-//     })
-//     //2. onsubmit handler -- validated so it's in the correct format
-//     function onSubmit(values: z.infer<typeof formSchema>) {
-//         //Do something with the form values
-//         // This will be type-safe and validated
-//         console.log(values);
-
-//         router.push(`/search/${values.input}`);
-//     }
-
-
-//   return (
-//     <Form {...form}>
-//         <form onSubmit={{form.handleSubmit(onSubmit)}} className="space-y-8">
-//             <FormField
-//                 control={form.control}
-//                 name='input'
-//                 render={({ field}) => (
-//                     <FormItem>
-//                         <FormControl>
-//                             <Input placeholder="Search..." {...field} />
-//                         </FormControl>
-//                     </FormItem>
-//                 )}
-//             />
-//         </form>
-//     </Form>
-
-
-//     // use a spread operator to pass in our form {...form}
-//   )
-// }
+// form docs: https://ui.shadcn.com/docs/components/form
