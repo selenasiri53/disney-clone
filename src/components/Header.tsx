7d82ec2d4928333db485c2550e5cf111f61fb8ad
selenasiri53 <selenasiri53@gmail.com>
2024-03-18 13:10:22 -0400

    search input bar using form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { ThemeToggler } from './ThemeToggler'
+import SearchInput from './SearchInput'
 
 const Header = () => {
   return (
-    <header>
-      <Link href="/">
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+      <Link href="/" className="mr-10">
         <Image
         src="https://links.papareact.com/a943ae"
         alt="Logo" 
@@ -17,8 +19,8 @@ const Header = () => {
 
       <div className="flex space-x-2">
         {/* GenreDropdown */}
-        {/* SearchInput */}
-        {/* ThemeToggler */}
+        <SearchInput />
+        <ThemeToggler />
       </div>
     </header>
   )
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form";
+// import * as z from "zod"
+import { z } from "zod"
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "./ui/input";
+
+// define form schema using zod index- validates info sent through form
+const formSchema = z.object({
+    input: z.string().min(2).max(50),
+})
+
+// formSchema goes outside
+// inside SearchInput function: define form schema (rules) -- input must be 2-50 chars
+// zod will validate the data, making it easier to find errors
+export default function SearchInput() { // redirects the user after search
+    const router = useRouter();
+
+    //1. define your form
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            input: "", // matches our schema
+        }
+    })
+
+    //2. onsubmit handler -- validated so it's in the correct format
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        //Do something with the form values
+        // This will be type-safe and validated
+        console.log(values);
+
+        router.push(`/search/${values.input}`);
+    }
+
+    return (
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name='input'
+                        render={({ field}) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input placeholder="Search..." {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
+          )
+}
+
+
+//     //1. define your form
+//     const form = useForm<z.infer<typeof formSchema>>({
+//         resolver: zodResolver(formSchema),
+//         defaultValues: {
+//             input: "", // matches our schema
+//         }
+//     })
+//     //2. onsubmit handler -- validated so it's in the correct format
+//     function onSubmit(values: z.infer<typeof formSchema>) {
+//         //Do something with the form values
+//         // This will be type-safe and validated
+//         console.log(values);
+
+//         router.push(`/search/${values.input}`);
+//     }
+
+
+//   return (
+//     <Form {...form}>
+//         <form onSubmit={{form.handleSubmit(onSubmit)}} className="space-y-8">
+//             <FormField
+//                 control={form.control}
+//                 name='input'
+//                 render={({ field}) => (
+//                     <FormItem>
+//                         <FormControl>
+//                             <Input placeholder="Search..." {...field} />
+//                         </FormControl>
+//                     </FormItem>
+//                 )}
+//             />
+//         </form>
+//     </Form>
+
+
+//     // use a spread operator to pass in our form {...form}
+//   )
+// }
